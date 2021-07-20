e347a479a741c2438be9cd791b835a1cff8e5c03
Iris-Hue <78930747+Iris-Hue@users.noreply.github.com>
2021-07-20 15:55:26 -0700

    add changes to height on overlay

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ let employees = [];
 const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US `
 const gridContainer = document.querySelector(".grid-container");
 const overlay = document.querySelector(".overlay");
-const modalContainer = document.querySelector(".modal-content");
+const modalContainer = document.querySelector(".modal-context");
 const modalClose = document.querySelector(".modal-close");
 
 
@@ -20,17 +20,17 @@ function displayEmployees(employeeData) {
     employees = employeeData;
 
 
-//store the employee HTML as we create it
+
 let employeeHTML = '';
 
-//loop through each employee and create HTML markup
+//loop through each employee
 employees.forEach((employee, index) => {
     let name = employee.name;
     let email = employee.email;
     let city = employee.location.city;
     let picture = employee.picture;
 
-    //template literals make this so much cleaner
+//template literals 
 employeeHTML += `
 <div class="card" data-index="${index}">
 <img class="avatar" src="${picture.large}"/>
@@ -44,7 +44,8 @@ employeeHTML += `
 });
 
 gridContainer.innerHTML = employeeHTML;
-    ///////////////search function////////////////////
+
+    ///////search function////////////////////
 
 const search = document.querySelector('#search');
 const boxTexts = document.querySelectorAll('.grid-container .card .name');
@@ -88,6 +89,7 @@ function displayModal(index) {
     overlay.classList.remove("hidden");
     modalContainer.innerHTML = modalHTML;
 }
+
 //---event Listeners---
 gridContainer.addEventListener('click', e => {
     //make sure the click is not on the gridContainer itself
